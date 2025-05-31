261407c3596ac39b731cf2c34a0018c6a44ea49f
Vishuu9090 <vishuu9090@gmail.com>
2025-05-31 14:51:07 +0530

    updated navbar

--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -3,70 +3,47 @@ import { FaBars } from "react-icons/fa";
 import { RxCross1 } from "react-icons/rx";
 import { Link } from 'react-scroll';
 
-
 export default function Navbar() {
-
     const Navmenu = [
         { href: "Skills", name: "About" },
         { href: "Portfolio", name: "Portfolio" },
         { href: "About", name: "Contact" },
-
-
     ]
 
     const [open, close] = useState(false)
 
-    const vishu = () => {
+    const toggleMenu = () => {
         close(!open)
     }
 
-
     return (
-        <nav className='flex justify-end px-10  relative h-10 items-end z-10'>
-            {/* <div className='font-semibold text-2xl text-amber-500'>
-                Vishal
-            </div> */}
-
-
-
-            <ul className=' hidden md:flex gap-10 text-amber-500 text-xl'>
-                {
-                    Navmenu.map((item, key) => (
-                        <Link key={key} to={item.href} smooth={true} duration={500}>{item.name}</Link>
-
-                    ))
-                }
-
+        <nav className='flex justify-end px-10 relative h-10 items-end z-10'>
+            <ul className='hidden md:flex gap-10 text-amber-500 text-xl'>
+                {Navmenu.map((item, key) => (
+                    <Link key={key} to={item.href} smooth={true} duration={500}>{item.name}</Link>
+                ))}
             </ul>
 
-            <div className=' block  md:hidden  ' onClick={vishu} >
-                {open ? <RxCross1 /> : <FaBars />}
+            <div className='block md:hidden text-amber-500' onClick={toggleMenu}>
+                {open ? <RxCross1 size={24} /> : <FaBars size={24} />}
             </div>
 
-            {
-                open && (
-                    <ul className=' md:hidden text-amber-600    absolute top-12 flex items-center flex-col bg-gray-900 w-full left-0 gap-3      '>
-                        {
-                            Navmenu.map((item, key) => (
-                        <Link key={key} to={item.href} smooth={true} duration={500}>{item.name}</Link>
-                            ))
-                        }
-                    </ul>
-                )
-            }
-
-
-
-
-
+            <div className={`md:hidden fixed top-0 left-0 w-full h-full bg-gray-900 transition-transform duration-300 ease-in-out ${open ? 'translate-x-0' : '-translate-x-full'}`}>
+                <ul className='text-amber-600 flex flex-col items-center justify-center h-full gap-8 text-xl'>
+                    {Navmenu.map((item, key) => (
+                        <Link 
+                            key={key} 
+                            to={item.href} 
+                            smooth={true} 
+                            duration={500}
+                            onClick={toggleMenu}
+                            className="hover:text-amber-400 transition-colors"
+                        >
+                            {item.name}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
         </nav>
-
     )
 }
-
-
-
-
-
-
-
