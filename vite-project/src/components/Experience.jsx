2de3258af85397fdff5fa3d4b25cf36ba8b512f7
Vishuu9090 <vishuu9090@gmail.com>
2025-06-02 21:37:16 +0530

    code changed

--- vite-project/src/components/Experience.jsx
+++ vite-project/src/components/Experience.jsx
@@ -1,18 +1,3 @@
-// import React from 'react'
-
-// export default function Experience() {
-//   return (
-//     <div className=' flex flex-col items-center mt-15 ' >
-//         <div className=' text-3xl md:text-3xl font-semibold text-white '>Experience</div>
-//         <div className=' border-2 border-amber-700 md:w-[400px] bg-gray-500 rounded-lg w-[250px] p-4 mt-6 '>
-//             <span className=' text-xl md:text-2xl font-semibold text-white'>Sqilco</span>
-//             <p className='text-white text-lg md:text-lg mt-2'>2024 - Present</p>
-//             <p className='text-amber-500  md:text-lg mt-1'>I have completed 6 month internship program in sqilco.</p>
-//         </div>
-//     </div>
-//   )
-// }
-
 import React from "react";
 import { motion } from "framer-motion";
 
--- vite-project/src/components/Project.jsx
+++ vite-project/src/components/Project.jsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-// import { FaGithub } from "react-icons/fa";
-// import img from "../assets/images/image2.png"
-
-// export default function Project() {
-//     return (
-//         <div className='flex flex-col-reverse md:flex-row md:items-center justify-evenly  mx-25 '>
-//             <div>
-//                 <div className=' text-2xl md:text-3xl text-white pl-15 pt-4 font-semibold'>Project #2</div>
-//                 <div className=' md:text-xl text-white pl-15'>Amazon clone.</div>
-//                 <div className='flex pl-15 pt-3'>
-//                     <button className=' rounded-lg px-1 md:text-lg mr-8 shadow-sm shadow-gray-900 bg-amber-600 hover:scale-110 transition-all duration-300 hover:bg-amber-700'><p className='text-white'>View Site</p></button>
-//                     <button className=' flex items-center justify-center shadow-sm shadow-gray-900 bg-amber-600 text-white hover:scale-110 transition-all duration-300 rounded-xl text-3xl  h-[40px] w-[40px] hover:bg-amber-700'><FaGithub /></button>
-//                 </div>
-//             </div>
-
-//             <div className=' shadow-xl shadow-gray-900 rounded-2xl' >
-//                 <img className='h-[250px] w-[400px] rounded-2xl ' src={img} alt="" />
-
-//             </div>
-//         </div>
-//     )
-// }
-
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { motion } from "framer-motion";
